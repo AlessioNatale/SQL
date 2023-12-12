884fbf167765c9c315221f8ce85de0db7d7f7e23
AlessioNatale <alessionatale12@gmail.com>
2023-12-12 17:32:55 +0100

    es-3

--- SQLlite.sql
+++ SQLlite.sql
@@ -1,3 +1,23 @@
+CREATE TABLE Books (
+	book_id INTEGER PRIMARY KEY,
+    title TEXT not NULL,
+    author TEXT NOT NULL,
+    genre TEXT NOT NULL,
+    published_year INTEGER NOT NULL,
+    isbn INTEGER NOT NULL,
+    price INTEGER NOT NULL,
+    rating INTEGER NOT NULL,
+    stock_count INTEGER NOT NULL
+)
+
+--Esercizio-2
+
+ALTER TABLE Books 
+ADD COLUMN pages INTEGER NOT NULL
+ADD COLUMN publisher TEXT NOT NULL
+
+--Esercizio-3
+
 INSERT INTO Books VALUES (1, 'A Journey to the Center', 'Jules Verne', 'Adventure', 1864, 1234567890123, 12.99, 4.5, 10, 350, 'Verne Publishing');
 INSERT INTO Books VALUES (2, 'War and Peace', 'Leo Tolstoy', 'Historical', 1869, 1234567890124, 14.99, 4.8, 5, 1200, 'Tolstoy Prints');
 INSERT INTO Books VALUES (3, 'Whispers of the Wind', 'Amelia Blackburn', 'Romance', 1982, 1234567890125, 9.99, 4.2, 20, 275, 'Blackburn');
